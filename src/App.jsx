080307e16779a3e698f0da723422898146657d2f
YagoPazos <yagopb@gmail.com>
2024-11-24 01:45:30 +0100

    starting on react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Layout from "./components/Layout";
+import Main from "./components/Main";
+import "./App.css";
+
+function App() {
+
+  return <Routes>
+    <Route path="/" element ={<Layout />}>
+      <Route index element={<Main />} />
+    </Route>
+  </Routes>;
+}
+
+export default App;
