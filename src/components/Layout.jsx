4a9844640d3ffded15cbe302d22c0b839652bc38
YagoPazos <yagopb@gmail.com>
2024-11-28 16:08:01 +0100

    styled projects section

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,13 +8,13 @@ function Layout() {
       <nav className="md:flex md:items-center md:justify-between bg-white px-4 md:px-16 nav items-center relative">
         <div className="flex justify-between items-center">
           <span>
-            <h4 className="logo text-xl md:text-3xl lg:text-xl">Yago Pazos</h4>
+            <h4 className="logo text-2xl">Yago Pazos</h4>
           </span>
           <span className="text-3xl cursor-pointer mx-2 md:hidden block">
             <ion-icon name="menu" onclick="Menu(this)"></ion-icon>
           </span>
         </div>
-        <ul className="md:flex md:items-center md:opacity-100 opacity-0 top-[-200px] transition-all ease-in duration-500 left-0 z-[-1] md:z-auto md:static bg-white align-middle absolute gap-10">
+        <ul className="md:flex md:items-center md:opacity-100 opacity-0 top-[-200px] transition-all ease-in duration-500 left-0 z-[-1] md:z-auto md:static bg-white align-middle absolute gap-8">
           <li>
             <a
               className="navlinks inline-block transform transition-transform duration-300 hover:scale-110"
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -18,18 +18,18 @@ import "../App.css";
 function Main() {
   return (
     <div>
-      <section className="max-w-2xl px-6 mx-auto flex flex-col md:flex-row p-4 justify-center mt-3 md:mt-14 mb-4 md:mb-32">
+      <section className="max-w-2xl px-7 mx-auto flex flex-col md:flex-row p-4 justify-center mt-3 md:mt-12 mb-4 md:mb-28">
         <div className="flex flex-col gap-2 mt-4">
           <div className="p-2">
             <h1 className="text-black navlinks text-4xl text-center md:text-left lg:text-left">
               Full-Stack Web Developer
             </h1>
-            <p className="md:max-w-80 text-sm text-black h3 text-left mt-4">
+            <p className="md:max-w-80 text-sm text-black h3 text-center md:text-left lg:text-left mt-4">
               Hi, I'm Yago, a Berlin-based web developer, focused on delivering
               high-quality websites.
             </p>
           </div>
-          <div className="flex flex-wrap p-2 mt-6 gap-4">
+          <div className="flex flex-wrap p-2 mt-5 gap-4">
             <div className="flex gap-4">
               <img
                 className="ts-icons duration-500"
@@ -84,130 +84,111 @@ function Main() {
         id="projects"
         className="w-full bg-white about flex flex-col justify-center"
       >
-        <div className="max-w-2xl px-8 pt-12 mx-auto">
+        <div className="max-w-2xl px-7 pt-12 mb-20 mx-auto">
           <h5 className="text-black h3 mb-20 lg:mb-4 text-4xl pt-4 text-left">
             Projects
           </h5>
           <div className="image-container">
             <img
-              className="mt-12 radius projectImage1"
-              src="https://i.imgur.com/FhK9YTY.png"
+              className="mt-12 radius"
+              src="https://i.imgur.com/Z3cvyZP.png"
               alt="Freebie It Project"
             />
           </div>
-          <div className="flex items-center">
-            <h4 className="text-black h3 text-2xl">Reduce, Reuse, Rehome</h4>
-            <img className="external-link-icon" src={linkIcon} alt="link icon" />
-            <a className="text-black h3" href="">open website</a>
+          <div className="flex items-center justify-between mt-5">
+            <h4 className="text-black h3 font-medium text-2xl">
+              Reduce, Reuse, Rehome
+            </h4>
+            <div className="flex flex-row justify-center items-center gap-1">
+              <img
+                className="external-link-icon"
+                src={linkIcon}
+                alt="link icon"
+              />
+              <a className="text-black text-sm h3" href="">
+                open website
+              </a>
+            </div>
           </div>
-          <p className="text-black h3 text-sm">
+          <p className="text-black h3 text-xs mt-3">
             A social network built with the MERN Stack, enabling users to share
             and discover locations where unwanted items are left on the street
             for reuse, promoting sustainability and reducing waste.
           </p>
-          <div>
-            <img
-              className="w-auto radius"
-              src="https://i.imgur.com/GhljBxr.png"
-              alt="Pokemon Project"
-            />
+          <img
+            className="mt-12 radius"
+            src="https://i.imgur.com/Si1Et1Y.png"
+            alt="Pokemon Project"
+          />
+          <div className="flex items-center justify-between mt-3">
+            <h4 className="text-black h3 font-medium text-2xl">
+              Find your Pokemon!
+            </h4>
+            <div className="flex flex-row items-center gap-1">
+              <img
+                className="external-link-icon"
+                src={linkIcon}
+                alt="link icon"
+              />
+              <a className="text-black text-sm h3" href="">
+                open website
+              </a>
+            </div>
           </div>
-          <div>
-            <h4>Find your Pokemon!</h4>
-            <img className="external-link-icon" src={linkIcon} alt="link icon" />
-            <a href="">open website</a>
-          </div>
-          <span>
+          <p className="text-black h3 text-xs mt-3">
             A Pokémon battle and Pokédex search app built with React, Node.js,
             Tailwind CSS, and PostgreSQL. Users can explore Pokémon details,
             simulate battles, and enjoy a dynamic, interactive interface.
-          </span>
+          </p>
         </div>
       </section>
       <section
         id="about"
-        className="flex flex-col md:flex-row lg:flex-row p-12 lg:p-40 md:p-20 xl:px-80 2xl:px-96 pb-2 sm:pb-4 w-full min-h-lvh"
+        className="w-full about flex flex-col justify-center items-center"
       >
-        <div className="flex flex-col justify-center md:w-1/2 lg:w-1/2 xl:w-1/2 2xl:w-1/2">
-          <h5 className="text-black h3 text-4xl pt-4 pl-0 md:pl-2 lg:pl-3 text-center md:text-left lg:text-left">
-            About
+        <div className="max-w-2xl px-7 pt-12 mb-28 mx-auto">
+          <h5 className="text-black h3 mb-20 lg:mb-4 text-4xl pt-4 text-left">
+            About me
           </h5>
-          <br />
-          <h5 className="text-black h3 text-xl pt-4 text-center md:text-left lg:text-left">
-            Full-Stack Developer based in Berlin
-          </h5>
-          <br />
-          <p className="text-black h3 pt-4 text-center md:text-left lg:text-left">
-            Hey, my name is Yago, Junior Full-Stack Developer. I'm passionate
-            about programming and have gained practical experience in both
-            frontend and backend development. I've worked with technologies like
-            TailwindCSS, HTML, CSS, JavaScript, and React, as well as backend
-            technologies like Node.js, Express, and databases such as MySQL and
-            MongoDB.
+          <p className="text-black h3 pt-4 text-sm text-center md:text-left lg:text-left">
+            As a Full-Stack Developer, I am passionate about building seamless
+            websites and applications. I have experience with frontend
+            technologies like TailwindCSS, HTML, CSS, JavaScript, and React, as
+            well as backend tools like Node.js, Express, MySQL, and MongoDB,
+            creating responsive and scalable solutions.
           </p>
-        </div>
-        <div className="border-b-2 pt-28 border-black"></div>
-        <div className="my-16 md:my-0 lg:my-0 mx-0 md:mx-12 lg:mx-32 divider2"></div>
-        <div className="flex justify-center items-center pb-28 md:pb-0 lg:pb-0 xl:pb-0 2xl:pb-0 md:w-1/2 lg:w-1/2 xl:w-1/2 2xl:w-1/2 relative">
           <img
-            className="w-auto"
-            src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZGV2ZWxvcGVyfGVufDB8fDB8fHww"
+            className="radius2 mt-8"
+            src="https://i.imgur.com/CIeIZak.png"
             alt="desktop-img"
           />
-          <img />
         </div>
       </section>
       <section
         id="contact"
-        className="bg-white flex flex-col p-12 lg:p-40 md:px-20 xl:px-80 2xl:px-96 w-full min-h-lvh"
+        className="bg-white w-full"
       >
-        <h5 className="text-black h3 text-4xl pt-4 pl-0 md:pl-2 lg:pl-3 text-center md:text-left lg:text-left">
-          Contact
-        </h5>
-        <br />
-        <div className="flex flex-col">
-          <h4 className="pt-20 pl-0 md:pl-8 lg:pl-8 xl:pl-8 2xl:pl-8 text-black h3 text-md pt-4 text-center md:text-left lg:text-left">
+        <div className="max-w-2xl px-7 mx-auto">
+          <h5 className="text-black h3 text-4xl pt-4 text-center md:text-left">
+            Contact
+          </h5>
+          <h4 className="pt-20 text-black h3 text-md pt-4 text-center md:text-left">
             I'd love to hear from you!
           </h4>
-          <br />
-          <div className="flex justify-center">
-            <div className="pt-12 flex flex-col md:flex-row lg:flex-row xl:flex-row 2xl:flex-row justify-center gap-6 md:gap-20 lg:gap-40 xl:gap-60 2xl:gap-96">
-              <div className="flex gap-8">
-                <div className="flex pl-0 gap-6 flex-row items-center">
-                  <img
-                    className="ts-icons duration-700"
-                    src={pinIcon}
-                    alt="Location"
-                  />
-                  <div>
-                    <h6 className="text-black h3 text-md pt-4 text-left font-semibold">
-                      Location
-                    </h6>
-                    <p className="text-black h3 text-md pt-4 text-left">
-                      Berlin, Germany
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="flex gap-8">
-                <div className="flex gap-6 flex-row items-center">
-                  <img
-                    className="ts-icons duration-700"
-                    src={emailIcon}
-                    alt="E-mail"
-                  />
-                  <div>
-                    <h6 className="text-black h3 text-md pt-4 text-left font-semibold">
-                      Mail
-                    </h6>
-                    <p className="text-black h3 text-md pt-4 text-left">
-                      yagopb@gmail.com
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <img className="ts-icons duration-700" src={pinIcon} alt="Location" />
+          <h6 className="text-black h3 text-md pt-4 text-left font-semibold">
+            Location
+          </h6>
+          <p className="text-black h3 text-md pt-4 text-left">
+            Berlin, Germany
+          </p>
+          <img className="ts-icons duration-700" src={emailIcon} alt="E-mail" />
+          <h6 className="text-black h3 text-md pt-4 text-left font-semibold">
+            Mail
+          </h6>
+          <p className="text-black h3 text-md pt-4 text-left">
+            yagopb@gmail.com
+          </p>
         </div>
       </section>
       <footer className="bg-black flex flex-col-reverse md:flex-row lg:flex-row justify-between p-12 lg:px-48 md:p-20 xl:px-80 2xl:px-96 pb-2 sm:pb-4 w-full">
