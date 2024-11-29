fa6525acbfe3232401b7923f38baf20ef821b9f9
YagoPazos <yagopb@gmail.com>
2024-11-29 11:15:25 +0100

    fixing responsive navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,53 +1,72 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
+import { useState, React } from "react";
+import { IoMenuOutline, IoCloseOutline } from "react-icons/io5";
 import "../App.css";
 
 function Layout() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="flex flex-col justify-center">
-      <nav className="md:flex md:items-center md:justify-between bg-white px-4 md:px-16 nav items-center relative">
-        <div className="flex justify-between items-center">
-          <span>
-            <h4 className="logo text-2xl">Yago Pazos</h4>
-          </span>
-          <span className="text-3xl cursor-pointer mx-2 md:hidden block">
-            <ion-icon name="menu" onclick="Menu(this)"></ion-icon>
-          </span>
+      <nav className="flex justify-between bg-white px-2 md:px-16 items-center relative">
+        <span className="logo text-2xl">Yago Pazos</span>
+        <div className="flex flex-row-reverse lg:flex-row">
+          <button
+            onClick={handleMenuToggle}
+            className="lg:hidden text-3xl ml-2"
+          >
+            {isMenuOpen ? (
+              <IoCloseOutline size={24} />
+            ) : (
+              <IoMenuOutline size={24} />
+            )}
+          </button>
+          <ul
+            className={`flex flex-col lg:flex-row bg-white items-center justify-start transition-all duration-300 overflow-hidden 
+          ${
+            isMenuOpen ? "max-h-[500px] opacity-100 gap-2" : "max-h-0 opacity-0"
+          } 
+          lg:max-h-full lg:opacity-100 lg:gap-4 w-full lg:w-auto`}
+          >
+            {" "}
+            <li>
+              <a
+                className="navlinks inline-block transform transition-transform duration-300 hover:scale-110"
+                href="/"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                className="navlinks inline-block transform transition-transform duration-300 hover:scale-110"
+                href="#projects"
+              >
+                Projects
+              </a>
+            </li>
+            <li>
+              <a
+                className="navlinks inline-block transform transition-transform duration-300 hover:scale-110"
+                href="#about"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                className="navlinks inline-block transform transition-transform duration-300 hover:scale-110"
+                href="#contact"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
         </div>
-        <ul className="md:flex md:items-center md:opacity-100 opacity-0 top-[-200px] transition-all ease-in duration-500 left-0 z-[-1] md:z-auto md:static bg-white align-middle absolute gap-8">
-          <li>
-            <a
-              className="navlinks inline-block transform transition-transform duration-300 hover:scale-110"
-              href="/"
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              className="navlinks inline-block transform transition-transform duration-300 hover:scale-110"
-              href="#projects"
-            >
-              Projects
-            </a>
-          </li>
-          <li>
-            <a
-              className="navlinks inline-block transform transition-transform duration-300 hover:scale-110"
-              href="#about"
-            >
-              About
-            </a>
-          </li>
-          <li>
-            <a
-              className="navlinks inline-block transform transition-transform duration-300 hover:scale-110"
-              href="#contact"
-            >
-              Contact
-            </a>
-          </li>
-        </ul>
       </nav>
       <Outlet />
     </div>
